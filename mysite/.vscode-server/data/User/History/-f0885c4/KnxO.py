14f6a158ad7b5bf0e2ac052d26a97b0ed1997c25
M.Aoyama <maruuca.com@gmail.com>
2024-02-12 20:21:04 +0900

    modelsの移行完了

---
+++ mysite/.vscode-server/data/User/History/-f0885c4/KnxO.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from .models.area import Area
+from .models.genre import Genre
+from .models.restaurant import Restaurant
+from django.utils.safestring import mark_safe
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image_preview') # 管理画面で表示するフィールド
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" width="100" />'.format(obj.image.url))
+        else:
+            return 'No Image'
+    image_preview.short_description = 'Image Preview' 
+    
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'name_alphabet', 'genre', 'address', 'area', 'phone', 'time', 'price_low', 'price_high', 'information', 'image')
+
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
