9c72650d865d4fc3e0c6345852eadc0ae5dfacc8
M.Aoyama <maruuca.com@gmail.com>
2024-02-13 21:28:48 +0900

    店舗一覧ページ追加

---
+++ mysite/.vscode-server/data/User/History/75dc3580/lspI.py
@@ -0,0 +1,13 @@
+from typing import Any
+from django.views.generic import TemplateView
+from commondb.models.restaurant import Restaurant
+
+class ShopListView(TemplateView):
+    template_name = "general/shop_list.html"
+    
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        # modelで定義した店舗のオブジェクトを取得
+        context['restaurants'] = Restaurant.objects.all()
+
+        return context
